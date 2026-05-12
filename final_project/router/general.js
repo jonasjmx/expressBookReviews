1c2d842a59a002eb51fbef059890d5a0273fe5d1
JONASJMX <jonasjmx@gmail.com>
2026-05-12 11:13:51 -0500

    implementando general.js con axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -20,48 +21,79 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(200).json(JSON.stringify(books));
+public_users.get('/', async function (req, res) {
+  try {
+    const getAllBooks = new Promise((resolve, reject) => {
+      resolve(books);
+    });
+    const bookList = await getAllBooks;
+    return res.status(200).json(JSON.stringify(bookList));
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book) {
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const getBook = new Promise((resolve, reject) => {
+      const book = books[isbn];
+      if (book) {
+        resolve(book);
+      } else {
+        reject(new Error("Book not found"));
+      }
+    });
+    const book = await getBook;
     return res.status(200).json(book);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-  return res.status(404).json({ message: "Book not found" });
- });
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const author = req.params.author;
-  const bookKeys = Object.keys(books);
-  const booksByAuthor = bookKeys
-    .filter(key => books[key].author === author)
-    .map(key => books[key]);
-  if (booksByAuthor.length > 0) {
-    return res.status(200).json(booksByAuthor);
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const author = req.params.author;
+    const getBooksByAuthor = new Promise((resolve, reject) => {
+      const bookKeys = Object.keys(books);
+      const result = bookKeys
+        .filter(key => books[key].author === author)
+        .map(key => books[key]);
+      if (result.length > 0) {
+        resolve(result);
+      } else {
+        reject(new Error("No books found for this author"));
+      }
+    });
+    const result = await getBooksByAuthor;
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-  return res.status(404).json({ message: "No books found for this author" });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  const title = req.params.title;
-  const bookKeys = Object.keys(books);
-  const booksByTitle = bookKeys
-    .filter(key => books[key].title === title)
-    .map(key => books[key]);
-  if (booksByTitle.length > 0) {
-    return res.status(200).json(booksByTitle);
+public_users.get('/title/:title', async function (req, res) {
+  try {
+    const title = req.params.title;
+    const getBooksByTitle = new Promise((resolve, reject) => {
+      const bookKeys = Object.keys(books);
+      const result = bookKeys
+        .filter(key => books[key].title === title)
+        .map(key => books[key]);
+      if (result.length > 0) {
+        resolve(result);
+      } else {
+        reject(new Error("No books found with this title"));
+      }
+    });
+    const result = await getBooksByTitle;
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
   }
-  return res.status(404).json({ message: "No books found with this title" });
 });
 
 //  Get book review
